af66e802aafd00b58a3a1aa055592e4a83073825
Wahyu Utomo <130083294+WagYu31@users.noreply.github.com>
2026-08-05 20:23:14 +0700

    Auto expand pin column in database tables to fix Data too long error for TEST001

--- ssll.id-giti.com/karyawan/process_attendance.php
+++ ssll.id-giti.com/karyawan/process_attendance.php
@@ -130,6 +130,10 @@ if (@file_put_contents($filepath, $imageBinary) === false) {
 }
 
 try {
+    // Auto-expand PIN column in DB if needed to accommodate longer NIPs / PINs (e.g. TEST001)
+    @$conn->query("ALTER TABLE absen_manual MODIFY COLUMN pin VARCHAR(50)");
+    @$conn->query("ALTER TABLE absen MODIFY COLUMN pin VARCHAR(50)");
+
     $query = "INSERT INTO absen_manual (
                 tgl_absen, tipe_absen, image, pin, nip, nama, lokasi_absen, lokasi_koordinat, ip, device_name, verif
               ) VALUES (
