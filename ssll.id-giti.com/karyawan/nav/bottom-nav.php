685e394baf8d375aedad272daea09f5a9959fde7
Wahyu Utomo <130083294+WagYu31@users.noreply.github.com>
2026-08-07 01:16:11 +0700

    Unify all employee pages with exact 5-item Mobile Bottom Navbar from Gambar 1 (Home, Absen, Presensi FAB, Gaji, Profil)

--- ssll.id-giti.com/karyawan/nav/bottom-nav.php
+++ ssll.id-giti.com/karyawan/nav/bottom-nav.php
@@ -1,18 +1,41 @@
-    <div class="custom-mobile-nav-wrapper d-lg-none">
-        <nav class="custom-mobile-nav">
-            <a href="home.php" class="custom-nav__link"> <i class="fa-solid fa-house"></i>
-                <span class="custom-nav__text">Home</span>
+<?php
+$current_page_basename = basename($_SERVER['PHP_SELF']);
+$nip_session = $_SESSION['nip'] ?? '';
+?>
+
+<!-- Link Mobile Bottom Navigation CSS -->
+<link rel="stylesheet" href="../assets/css/bottom-nav.css?v=<?php echo time(); ?>">
+
+<!-- Unified 5-Item Mobile Bottom Navigation Bar -->
+<div class="custom-mobile-nav-wrapper d-lg-none">
+    <nav class="custom-mobile-nav">
+        <a href="home.php" class="custom-nav__link <?php echo ($current_page_basename == 'home.php') ? 'active' : ''; ?>">
+            <i class="fa-solid fa-house-chimney-user"></i>
+            <span class="custom-nav__text">Home</span>
+        </a>
+
+        <a href="riwayat-absen.php" class="custom-nav__link <?php echo ($current_page_basename == 'riwayat-absen.php' || $current_page_basename == 'absensi.php') ? 'active' : ''; ?>">
+            <i class="fa-solid fa-clock"></i>
+            <span class="custom-nav__text">Absen</span>
+        </a>
+
+        <!-- Center Floating FAB Absen Camera Button -->
+        <div class="custom-nav__fab-container">
+            <a href="absen.php?nik=<?php echo htmlspecialchars($nip_session); ?>#form-absen" class="custom-nav__fab-button" title="Absen Masuk">
+                <i class="fa-solid fa-camera"></i>
             </a>
-            <div class="custom-nav__fab-container">
-                <a href="absensi.php" class="custom-nav__fab-button" aria-label="Absen">
-                <!--<a href="absen.php?nik=<?php echo htmlspecialchars($nik); ?>" class="custom-nav__fab-button" aria-label="Absen">-->
-                    <i class="fa-solid fa-hand-point-up"></i> </a>
-                <span class="custom-nav__fab-text">Absen</span>
-            </div>
-            <a href="profile.php" class="custom-nav__link">
-                <i class="fa-solid fa-user"></i>
-                <span class="custom-nav__text">Profile</span>
-            </a>
-        </nav>
-    </div>
-    <script src="../assets/js/pwa-install.js?v=<?php echo time(); ?>"></script>
+            <span class="custom-nav__fab-text">Presensi</span>
+        </div>
+
+        <a href="riwayat-gaji.php" class="custom-nav__link <?php echo ($current_page_basename == 'riwayat-gaji.php') ? 'active' : ''; ?>">
+            <i class="fa-solid fa-file-invoice-dollar"></i>
+            <span class="custom-nav__text">Gaji</span>
+        </a>
+
+        <a href="profile.php" class="custom-nav__link <?php echo ($current_page_basename == 'profile.php' || $current_page_basename == 'edit-profile.php') ? 'active' : ''; ?>">
+            <i class="fa-solid fa-id-card-clip"></i>
+            <span class="custom-nav__text">Profil</span>
+        </a>
+    </nav>
+</div>
+<script src="../assets/js/pwa-install.js?v=<?php echo time(); ?>"></script>
